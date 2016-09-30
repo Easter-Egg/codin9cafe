d53bef01ce6b4103012906382dcb1f8a6893a09d
Taewoo Kim <tu_k@naver.com>
2016-10-01 00:48:10 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ angular.module('codin9cafe',
 .run(function(){
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDdCYEfm6anwaTzis1rZeVD0KueNVqKJ5Q",
-    authDomain: "codin9cafe-80eea.firebaseapp.com",
-    databaseURL: "https://codin9cafe-80eea.firebaseio.com",
-    storageBucket: "codin9cafe-80eea.appspot.com",
-    messagingSenderId: "627597171799"
+    apiKey: "",
+    authDomain: ",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
 })
