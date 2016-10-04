f8d8bebb40783d4fe877edd9ccbd8a82f5482f17
Taewoo Kim <tu_k@naver.com>
2016-10-04 16:45:31 +0900

    161004

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,12 @@ angular.module('codin9cafe.controllers', [])
 .controller('mainCtrl',  function($scope, $timeout, $http, $mdDialog, TimeTable) {
   $scope.noMoreSeminars = false;
   $scope.items = [];
+  $scope.lastEventNum = 0;
+
+  var eventsRef = firebase.database().ref('events/');
+  eventsRef.on('child_added', function(data){
+    $scope.lastEventNum = data.val().num;
+  });
 
   // asyncly get a added seminars from firebase database
   TimeTable.init().then(function(data){
@@ -33,7 +39,7 @@ angular.module('codin9cafe.controllers', [])
 
   $scope.addSeminarPrompt = function(ev) {
     $mdDialog.show({
-      controller: dialogController,
+      controller: addSeminarDialogController,
       templateUrl: 'templates/addSeminarDialog.tmpl.html',
       parent: angular.element(document.body),
       targetEvent: ev,
@@ -46,8 +52,8 @@ angular.module('codin9cafe.controllers', [])
     });
   };
 
-  function dialogController($scope, $mdDialog) {
-    $scope.today = new Date();
+  function addSeminarDialogController($scope, $mdDialog) {
+    var today = new Date();
     $scope.hide = function() {
       $mdDialog.hide();
     };
@@ -60,10 +66,10 @@ angular.module('codin9cafe.controllers', [])
       var newSeminarKey = firebase.database().ref().child('seminars').push().key;
       firebase.database().ref('seminars/' + newSeminarKey).set({
         key: newSeminarKey,
-        date: $scope.today,
-        title: $scope.seminar.title,
-        speaker: $scope.seminar.speaker,
-        content : $scope.seminar.content,
+        date: today,
+        title: seminar.title,
+        speaker: seminar.speaker,
+        content : seminar.content,
         time: 0,
         like: 0
       }).then(function(){
@@ -77,6 +83,55 @@ angular.module('codin9cafe.controllers', [])
             .textContent('Check your browser console!')
             .ariaLabel('Error')
             .ok('Got it!')
+            .targetEvent(ev)
+        );
+      });
+    };
+  }
+
+  $scope.makeEventPrompt = function(ev) {
+    $mdDialog.show({
+      controller: makeEventDialogController,
+      templateUrl: 'templates/makeEventDialog.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: false
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+
+  function makeEventDialogController($scope, $mdDialog) {
+    var today = new Date();
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.makeEvent = function(event){
+      var newEventKey = firebase.database().ref().child('events').push().key;
+      firebase.database().ref('events/' + newEventKey).set({
+        key: newEventKey,
+        date: event.date,
+        num: event.num,
+        loc: event.location
+      }).then(function(){
+        $mdDialog.hide();
+      }, function(msg){
+        $mdDialog.show(
+          $mdDialog.alert()
+            .parent(angular.element(document.querySelector('#popupContainer')))
+            .clickOutsideToClose(true)
+            .title('Error')
+            .textContent('Check your browser console!')
+            .ariaLabel('Error')
+            .ok('Close')
             .targetEvent(ev)
         );
       });
--- js/services.js
+++ js/services.js
@@ -4,9 +4,10 @@ angular.module('codin9cafe.services', [])
   		init: function(){
   			var deferrd = $q.defer();
   			var items = [];
-  			var recentSeminarsRef = firebase.database().ref('seminars/').limitToLast(3);
+        var lastEventKey = firebase.database().ref('events/').limitToLast(1).key;
+        var seminarsOfLastEvents = firebase.database().ref('events/' + lastEventKey + '/seminars/');
 				var deferred = $q.defer();
-				recentSeminarsRef.once('value').then(function(datas){
+				seminarsOfLastEvents.once('value').then(function(datas){
 					datas.forEach(function(data){
 					  items.splice(0, 0, data.val());
 					});
